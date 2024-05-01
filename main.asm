c905320a7ffc57350c32e0dbe27bd1c1c2ae8d68
javialroro <102406440+javialroro@users.noreply.github.com>
2024-05-01 15:36:28 -0600

    init

---
+++ main.asm
@@ -0,0 +1,21 @@
+include C:\irvine\Irvine32.inc
+
+.data
+    mensaje byte "Ingrese los valores del polinomio separados por una coma y al terminar incluya un punto",0
+    buffer byte 100 DUP(?)
+
+.code
+main PROC
+    mov edx, OFFSET mensaje
+    call WriteString
+    call Crlf
+
+    mov edx, OFFSET buffer ; Load address of buffer
+    mov ecx, 100            ; Maximum number of characters to read
+    call ReadString         ; Read string from user
+
+    ; Your code to process the input goes here
+    
+    exit
+main ENDP
+END main
